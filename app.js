6ee46f24308fab64c60b283aef8ecfa2042b7cc1
Aditya <akaditya394@gmail.com>
2022-06-28 16:04:01 +0530

    special attack feature added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
 
@@ -17,11 +18,22 @@ const app = Vue.createApp({
     playerHealthStyle() {
       return { width: this.playerHealth + "%" };
     },
+    canSpecialAttackMonster() {
+      return this.currentRound % 3 !== 0;
+    },
   },
 
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomNumber(12, 5);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomNumber(20, 10);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
